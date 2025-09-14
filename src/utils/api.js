50060dc13cb7bfb9319bb4a53bdeb53aa34b1c8a
yurshtinkai <lourdangeloubufete17@gmail.com>
2025-09-14 13:57:20 +0800

    fix faild deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,5 +17,5 @@ export const API_BASE_URL = getApiUrl();
 console.log('API_BASE_URL configured as:', API_BASE_URL);
 console.log('REACT_APP_API_URL from env:', process.env.REACT_APP_API_URL);
 
-// export const getSessionToken = () => localStorage.getItem('sessionToken');
-// export const getUserRole = () => localStorage.getItem('userRole');
+export const getSessionToken = () => localStorage.getItem('sessionToken');
+export const getUserRole = () => localStorage.getItem('userRole');
